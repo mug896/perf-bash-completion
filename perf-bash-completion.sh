d80c0e0eb10a886f3c81807f7b7750963f9fbbb4
Hyunho Cho <mug896@gmail.com>
2022-10-31 03:55:54 +0900

    update

--- perf-bash-completion.sh
+++ perf-bash-completion.sh
@@ -192,7 +192,7 @@ _perf()
     # It is recommended that every completion function starts with _init_comp_wordbreaks,
     # whether or not they change the COMP_WORDBREAKS variable afterward.
     _init_comp_wordbreaks
-    [[ $COMP_WORDBREAKS == *:* ]] && COMP_WORDBREAKS=${COMP_WORDBREAKS/:/}
+    COMP_WORDBREAKS=${COMP_WORDBREAKS//:/}
     [[ $COMP_WORDBREAKS != *,* ]] && COMP_WORDBREAKS+=","
 
     local extglob_reset=$(shopt -p extglob)
