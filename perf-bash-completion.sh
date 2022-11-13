d69e28221ecb89b85d1f4494250619fafb3dfaf1
Hyunho Cho <mug896@gmail.com>
2022-11-13 12:20:08 +0900

    update

--- perf-bash-completion.sh
+++ perf-bash-completion.sh
@@ -190,7 +190,7 @@ _init_comp_wordbreaks()
 _perf() 
 {
     # It is recommended that all completion functions start with _init_comp_wordbreaks,
-    # regardless of whether you change the COMP_WORDBREAKS variable afterword.
+    # regardless of whether you change the COMP_WORDBREAKS variable afterward.
     _init_comp_wordbreaks
     COMP_WORDBREAKS=${COMP_WORDBREAKS//:/}
     [[ $COMP_WORDBREAKS != *,* ]] && COMP_WORDBREAKS+=","
